79e8babd8af2740ce3e645f542a1e866626b132c
jawadbabar <jawad@berkeley.edu>
2021-12-23 16:04:24 -0800

    empty tasks debugging

--- empty_tasks.py
+++ empty_tasks.py
@@ -3,9 +3,18 @@ import time
 
 ray.init()
 
+# empty task
 @ray.remote
 def empty_fn():
     return
+
+# empty actor
+@ray.remote
+class EmptyActor:
+    def __init__(self, fns):
+        self.returns = [fn.remote() for fn in fns]
+    def result(self):
+        return returns
 
 # antipattern
 # s = time.time()
@@ -14,13 +23,31 @@ def empty_fn():
 #     returns.append(ray.get(empty_fn.remote()))
 # print("Antipattern: " + str(time.time() - s))
 
-# better approach
-ns = [10000, 12000, 14000, 16000, 18000, 20000]
-for n in ns:
-    s = time.time()
-    refs = []
-    for i in range(n):
-        refs.append(empty_fn.remote())
+# total number of empty tasks that take a little more than 1s
+n = 18000
+# n = 2000
 
-    returns = ray.get(refs)
-    print("Empty tasks (" + str(n) + "): "+ str(time.time() - s))
+# empty tasks
+s = time.time()
+refs = []
+for i in range(n):
+    refs.append(empty_fn.remote())
+returns = ray.get(refs)
+print("Empty tasks (" + str(n) + "): "+ str(time.time() - s))
+
+
+# empty actors
+s = time.time()
+
+fns = [empty_fn for _ in range(n)]
+# results = []
+BATCH_SIZE = 500
+for i in range(0, len(fns), BATCH_SIZE):
+    batch = fns[i : i + BATCH_SIZE]
+    actor = EmptyActor.remote(batch)
+    ray.get(actor.result.remote())
+
+# ray.get(returns)
+
+
+print("Actors with empty tasks (" + str(n) + "): "+ str(time.time() - s))
