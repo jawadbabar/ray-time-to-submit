2427fa75adfa23ce5592bf3b56d27b95b87af168
jawadbabar <jawad@berkeley.edu>
2021-12-27 11:59:03 -0800

    adding back warnings

--- empty_tasks_v2.py
+++ empty_tasks_v2.py
@@ -2,7 +2,7 @@ import ray
 import time
 import logging
 
-ray.init(num_cpus=32, log_to_driver=False, logging_level=logging.FATAL) # removing warnings
+ray.init(num_cpus=32) # removing warnings
 
 # empty task
 @ray.remote
@@ -27,7 +27,7 @@ print("Empty tasks (# tasks: " + str(n) + "): "+ str(end - start))
 
 # empty actors
 fns = [empty_fn for _ in range(n)]
-for a in [1, 2, 4, 8, 16, 32, 64, 128]:
+for a in [1, 2, 4, 8, 16, 32]:
 
     batch_size = n // a
 
